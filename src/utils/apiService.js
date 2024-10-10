3547337614eb8a59aefd7640025873af5544fede
Shakhzod <forworkingappsinmylaptop@gmail.com>
2024-10-10 17:48:53 +0500

    first commit

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -1,23 +1,23 @@
-import createApiClient from './apiClient';
+import createApiClient from "./apiClient";
 
-import { API_KEY } from './constants'; // Import the API key constant
+import { API_KEY } from "./constants"; // Import the API key constant
 
 // Initialize the API client with your API key
 const apiClient = createApiClient(API_KEY);
 
 const ApiService = {
   getFaqs: async () => {
-    const response = await apiClient.get('/faqs');
+    const response = await apiClient.get("/faqs");
     return response.data;
   },
 
   getAppSettings: async () => {
-    const response = await apiClient.get('/app-settings');
+    const response = await apiClient.get("/app-settings");
     return response.data;
   },
 
   getArticles: async () => {
-    const response = await apiClient.get('/articles');
+    const response = await apiClient.get("/articles");
     return response.data;
   },
 
@@ -27,61 +27,74 @@ const ApiService = {
   },
 
   submitContactForm: async (name, mobile, email, message) => {
-    const response = await apiClient.post('/contacts', { name, mobile, email, message });
+    const response = await apiClient.post("/contacts", {
+      name,
+      mobile,
+      email,
+      message,
+    });
     return response.data;
   },
 
   getListings: async (cityId) => {
-    const response = await apiClient.get('/listings', { params: { city_id: cityId } });
+    const response = await apiClient.get("/listings", {
+      params: { city_id: cityId },
+    });
     return response.data;
   },
 
   getListingsBySpecialist: async (cityId, specialistId) => {
-    const response = await apiClient.get('/listings', {
+    const response = await apiClient.get("/listings", {
       params: { city_id: cityId, specialist_id: specialistId },
     });
     return response.data;
   },
 
   getListingsByTreatment: async (cityId, treatmentId) => {
-    const response = await apiClient.get('/listings', {
+    const response = await apiClient.get("/listings", {
       params: { city_id: cityId, treatment_id: treatmentId },
     });
     return response.data;
   },
-
+  // Search input & change placeholder 
   searchListings: async (query, cityId) => {
-    const response = await apiClient.get('/listings', { params: { search: query, city_id: cityId } });
+    const response = await apiClient.get("/listings", {
+      params: { search: query, city_id: cityId },
+    });
     return response.data;
   },
-
+//
   getCities: async (districtId) => {
-    const response = await apiClient.get('/cities', { params: { district_id: districtId } });
+    const response = await apiClient.get("/cities", {
+      params: { district_id: districtId },
+    });
     return response.data;
   },
 
   getDistricts: async (stateId) => {
-    const response = await apiClient.get('/districts', { params: { state_id: stateId } });
+    const response = await apiClient.get("/districts", {
+      params: { state_id: stateId },
+    });
     return response.data;
   },
 
   getStates: async () => {
-    const response = await apiClient.get('/states');
+    const response = await apiClient.get("/states");
     return response.data;
   },
 
   getSpecialists: async () => {
-    const response = await apiClient.get('/specialists');
+    const response = await apiClient.get("/specialists");
     return response.data;
   },
 
   getTreatments: async () => {
-    const response = await apiClient.get('/treatments');
+    const response = await apiClient.get("/treatments");
     return response.data;
   },
 
   getEducation: async () => {
-    const response = await apiClient.get('/education');
+    const response = await apiClient.get("/education");
     return response.data;
   },
 };
